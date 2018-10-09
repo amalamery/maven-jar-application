00d258b93d54ae7e5d04eb34ff7b9833242ba333
Shyjy,Amala <amalamery-varghese@mail.nisaan.co.jp>
2018-10-09 13:49:22 +0530

    pom and app modified

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,16 @@ package com.mycompany.app;
  * Hello world!
  *
  */
+ import java.util.*;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int a,b;
+		Scanner in = new Scanner(System.in);
+		a = in.nextInt();
+		b= in.nextInt();
+		int sum = a + b;
+		System.out.println( "Sum: " + sum );
     }
 }
